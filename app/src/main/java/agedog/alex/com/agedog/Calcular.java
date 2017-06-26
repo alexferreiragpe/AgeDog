b5141e76be8e6e956ee3036bec13c233f23d5e3a
Alex Ferreira <alexferreiracp@gmail.com>
2017-06-26 11:46:51 -0300

    correção layout

--- app/src/main/java/agedog/alex/com/agedog/Calcular.java
+++ app/src/main/java/agedog/alex/com/agedog/Calcular.java
@@ -9,5 +9,6 @@ public class Calcular extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calcular);
+        setTitle("AgeOfDog Como Calcular");
     }
 }
--- app/src/main/java/agedog/alex/com/agedog/MainActivity.java
+++ app/src/main/java/agedog/alex/com/agedog/MainActivity.java
@@ -21,11 +21,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setTitle("AgeOfDog - Idade Canina x Humana");
+
         Spinner CbIdade = (Spinner) findViewById(R.id.cbIdade);
         Spinner CbRaca = (Spinner) findViewById(R.id.tipo);
 
-        this.idade = new String[]{"Selecione", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16"};
-        this.raca = new String[]{"Selecione", "Pequena", "Media", "Grande"};
+        this.idade = new String[]{"Selecione Idade", "1 Ano", "2 Anos", "3 Anos", "4 Anos", "5 Anos", "6 Anos", "7 Anos", "8 Anos", "9 Anos", "10 Anos", "11 Anos", "12 Anos", "13 Anos", "14 Anos", "15 Anos", "16 Anos"};
+        this.raca = new String[]{"Selecione Porte ", "Pequeno", "Médio", "Grande"};
 
         ArrayAdapter<String> aa = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, raca);
         aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -89,81 +91,81 @@ public class MainActivity extends AppCompatActivity {
         Spinner CbRaca = (Spinner) findViewById(R.id.tipo);
         TextView txtExibeIdade = (TextView) findViewById(R.id.txtExibeIdade);
 
-        if ((CbRaca.getSelectedItem() == "Pequena" || CbRaca.getSelectedItem() == "Media" || CbRaca.getSelectedItem() == "Grande") && (CbIdade.getSelectedItem() == ("1"))) {
+        if ((CbRaca.getSelectedItem() == "Pequeno" || CbRaca.getSelectedItem() == "Médio" || CbRaca.getSelectedItem() == "Grande") && (CbIdade.getSelectedItem() == ("1 Ano"))) {
             txtExibeIdade.setText("Estimativa: 15 anos");
-        } else if ((CbRaca.getSelectedItem() == "Pequena" || CbRaca.getSelectedItem() == "Media" || CbRaca.getSelectedItem() == "Grande") && (CbIdade.getSelectedItem() == ("2"))) {
+        } else if ((CbRaca.getSelectedItem() == "Pequeno" || CbRaca.getSelectedItem() == "Médio" || CbRaca.getSelectedItem() == "Grande") && (CbIdade.getSelectedItem() == ("2 Anos"))) {
             txtExibeIdade.setText("Estimativa: 24 anos");
-        } else if ((CbRaca.getSelectedItem() == "Pequena" || CbRaca.getSelectedItem() == "Media" || CbRaca.getSelectedItem() == "Grande") && (CbIdade.getSelectedItem() == ("3"))) {
+        } else if ((CbRaca.getSelectedItem() == "Pequeno" || CbRaca.getSelectedItem() == "Médio" || CbRaca.getSelectedItem() == "Grande") && (CbIdade.getSelectedItem() == ("3 Anos"))) {
             txtExibeIdade.setText("Estimativa: 28 anos");
-        } else if ((CbRaca.getSelectedItem() == "Pequena" || CbRaca.getSelectedItem() == "Media" || CbRaca.getSelectedItem() == "Grande") && (CbIdade.getSelectedItem() == ("4"))) {
+        } else if ((CbRaca.getSelectedItem() == "Pequeno" || CbRaca.getSelectedItem() == "Médio" || CbRaca.getSelectedItem() == "Grande") && (CbIdade.getSelectedItem() == ("4 Anos"))) {
             txtExibeIdade.setText("Estimativa: 32 anos");
-        } else if ((CbRaca.getSelectedItem() == "Pequena" || CbRaca.getSelectedItem() == "Media" || CbRaca.getSelectedItem() == "Grande") && (CbIdade.getSelectedItem() == ("5"))) {
+        } else if ((CbRaca.getSelectedItem() == "Pequeno" || CbRaca.getSelectedItem() == "Médio" || CbRaca.getSelectedItem() == "Grande") && (CbIdade.getSelectedItem() == ("5 Anos"))) {
             txtExibeIdade.setText("Estimativa: 36 anos");
-        } else if ((CbRaca.getSelectedItem() == "Pequena") && (CbIdade.getSelectedItem() == ("6"))) {
+        } else if ((CbRaca.getSelectedItem() == "Pequeno") && (CbIdade.getSelectedItem() == ("6 Anos"))) {
             txtExibeIdade.setText("Estimativa: 40 anos");
-        } else if ((CbRaca.getSelectedItem() == "Pequena") && (CbIdade.getSelectedItem() == ("7"))) {
+        } else if ((CbRaca.getSelectedItem() == "Pequeno") && (CbIdade.getSelectedItem() == ("7 Anos"))) {
             txtExibeIdade.setText("Estimativa: 44 anos");
-        } else if ((CbRaca.getSelectedItem() == "Pequena") && (CbIdade.getSelectedItem() == ("8"))) {
+        } else if ((CbRaca.getSelectedItem() == "Pequeno") && (CbIdade.getSelectedItem() == ("8 Anos"))) {
             txtExibeIdade.setText("Estimativa: 48 anos");
-        } else if ((CbRaca.getSelectedItem() == "Pequena") && (CbIdade.getSelectedItem() == ("9"))) {
+        } else if ((CbRaca.getSelectedItem() == "Pequeno") && (CbIdade.getSelectedItem() == ("9 Anos"))) {
             txtExibeIdade.setText("Estimativa: 52 anos");
-        } else if ((CbRaca.getSelectedItem() == "Pequena") && (CbIdade.getSelectedItem() == ("10"))) {
+        } else if ((CbRaca.getSelectedItem() == "Pequeno") && (CbIdade.getSelectedItem() == ("10 Anos"))) {
             txtExibeIdade.setText("Estimativa: 56 anos");
-        } else if ((CbRaca.getSelectedItem() == "Pequena") && (CbIdade.getSelectedItem() == ("11"))) {
+        } else if ((CbRaca.getSelectedItem() == "Pequeno") && (CbIdade.getSelectedItem() == ("11 Anos"))) {
             txtExibeIdade.setText("Estimativa: 60 anos");
-        } else if ((CbRaca.getSelectedItem() == "Pequena") && (CbIdade.getSelectedItem() == ("12"))) {
+        } else if ((CbRaca.getSelectedItem() == "Pequeno") && (CbIdade.getSelectedItem() == ("12 Anos"))) {
             txtExibeIdade.setText("Estimativa: 64 anos");
-        } else if ((CbRaca.getSelectedItem() == "Pequena") && (CbIdade.getSelectedItem() == ("13"))) {
+        } else if ((CbRaca.getSelectedItem() == "Pequeno") && (CbIdade.getSelectedItem() == ("13 Anos"))) {
             txtExibeIdade.setText("Estimativa: 68 anos");
-        } else if ((CbRaca.getSelectedItem() == "Pequena") && (CbIdade.getSelectedItem() == ("14"))) {
+        } else if ((CbRaca.getSelectedItem() == "Pequeno") && (CbIdade.getSelectedItem() == ("14 Anos"))) {
             txtExibeIdade.setText("Estimativa: 72 anos");
-        } else if ((CbRaca.getSelectedItem() == "Pequena") && (CbIdade.getSelectedItem() == ("15"))) {
+        } else if ((CbRaca.getSelectedItem() == "Pequeno") && (CbIdade.getSelectedItem() == ("15 Anos"))) {
             txtExibeIdade.setText("Estimativa: 76 anos");
-        } else if ((CbRaca.getSelectedItem() == "Pequena") && (CbIdade.getSelectedItem() == ("16"))) {
+        } else if ((CbRaca.getSelectedItem() == "Pequeno") && (CbIdade.getSelectedItem() == ("16 Anos"))) {
             txtExibeIdade.setText("Estimativa: 80 anos");
-        } else if ((CbRaca.getSelectedItem() == "Media") && (CbIdade.getSelectedItem() == ("6"))) {
+        } else if ((CbRaca.getSelectedItem() == "Médio") && (CbIdade.getSelectedItem() == ("6 Anos"))) {
             txtExibeIdade.setText("Estimativa: 42 anos");
-        } else if ((CbRaca.getSelectedItem() == "Media") && (CbIdade.getSelectedItem() == ("7"))) {
+        } else if ((CbRaca.getSelectedItem() == "Médio") && (CbIdade.getSelectedItem() == ("7 Anos"))) {
             txtExibeIdade.setText("Estimativa: 47 anos");
-        } else if ((CbRaca.getSelectedItem() == "Media") && (CbIdade.getSelectedItem() == ("8"))) {
+        } else if ((CbRaca.getSelectedItem() == "Médio") && (CbIdade.getSelectedItem() == ("8 Anos"))) {
             txtExibeIdade.setText("Estimativa: 51 anos");
-        } else if ((CbRaca.getSelectedItem() == "Media") && (CbIdade.getSelectedItem() == ("9"))) {
+        } else if ((CbRaca.getSelectedItem() == "Médio") && (CbIdade.getSelectedItem() == ("9 Anos"))) {
             txtExibeIdade.setText("Estimativa: 56 anos");
-        } else if ((CbRaca.getSelectedItem() == "Media") && (CbIdade.getSelectedItem() == ("10"))) {
+        } else if ((CbRaca.getSelectedItem() == "Médio") && (CbIdade.getSelectedItem() == ("10 Anos"))) {
             txtExibeIdade.setText("Estimativa: 60 anos");
-        } else if ((CbRaca.getSelectedItem() == "Media") && (CbIdade.getSelectedItem() == ("11"))) {
+        } else if ((CbRaca.getSelectedItem() == "Médio") && (CbIdade.getSelectedItem() == ("11 Anos"))) {
             txtExibeIdade.setText("Estimativa: 65 anos");
-        } else if ((CbRaca.getSelectedItem() == "Media") && (CbIdade.getSelectedItem() == ("12"))) {
+        } else if ((CbRaca.getSelectedItem() == "Médio") && (CbIdade.getSelectedItem() == ("12 Anos"))) {
             txtExibeIdade.setText("Estimativa: 69 anos");
-        } else if ((CbRaca.getSelectedItem() == "Media") && (CbIdade.getSelectedItem() == ("13"))) {
+        } else if ((CbRaca.getSelectedItem() == "Médio") && (CbIdade.getSelectedItem() == ("13 Anos"))) {
             txtExibeIdade.setText("Estimativa: 74 anos");
-        } else if ((CbRaca.getSelectedItem() == "Media") && (CbIdade.getSelectedItem() == ("14"))) {
+        } else if ((CbRaca.getSelectedItem() == "Médio") && (CbIdade.getSelectedItem() == ("14 Anos"))) {
             txtExibeIdade.setText("Estimativa: 78 anos");
-        } else if ((CbRaca.getSelectedItem() == "Media") && (CbIdade.getSelectedItem() == ("15"))) {
+        } else if ((CbRaca.getSelectedItem() == "Médio") && (CbIdade.getSelectedItem() == ("15 Anos"))) {
             txtExibeIdade.setText("Estimativa: 83 anos");
-        } else if ((CbRaca.getSelectedItem() == "Media") && (CbIdade.getSelectedItem() == ("16"))) {
+        } else if ((CbRaca.getSelectedItem() == "Médio") && (CbIdade.getSelectedItem() == ("16 Anos"))) {
             txtExibeIdade.setText("Estimativa: 87 anos");
-        } else if ((CbRaca.getSelectedItem() == "Grande") && (CbIdade.getSelectedItem() == ("6"))) {
+        } else if ((CbRaca.getSelectedItem() == "Grande") && (CbIdade.getSelectedItem() == ("6 Anos"))) {
             txtExibeIdade.setText("Estimativa: 45 anos");
-        } else if ((CbRaca.getSelectedItem() == "Grande") && (CbIdade.getSelectedItem() == ("7"))) {
+        } else if ((CbRaca.getSelectedItem() == "Grande") && (CbIdade.getSelectedItem() == ("7 Anos"))) {
             txtExibeIdade.setText("Estimativa: 50 anos");
-        } else if ((CbRaca.getSelectedItem() == "Grande") && (CbIdade.getSelectedItem() == ("8"))) {
+        } else if ((CbRaca.getSelectedItem() == "Grande") && (CbIdade.getSelectedItem() == ("8 Anos"))) {
             txtExibeIdade.setText("Estimativa: 55 anos");
-        } else if ((CbRaca.getSelectedItem() == "Grande") && (CbIdade.getSelectedItem() == ("9"))) {
+        } else if ((CbRaca.getSelectedItem() == "Grande") && (CbIdade.getSelectedItem() == ("9 Anos"))) {
             txtExibeIdade.setText("Estimativa: 61 anos");
-        } else if ((CbRaca.getSelectedItem() == "Grande") && (CbIdade.getSelectedItem() == ("10"))) {
+        } else if ((CbRaca.getSelectedItem() == "Grande") && (CbIdade.getSelectedItem() == ("10 Anos"))) {
             txtExibeIdade.setText("Estimativa: 66 anos");
-        } else if ((CbRaca.getSelectedItem() == "Grande") && (CbIdade.getSelectedItem() == ("11"))) {
+        } else if ((CbRaca.getSelectedItem() == "Grande") && (CbIdade.getSelectedItem() == ("11 Anos"))) {
             txtExibeIdade.setText("Estimativa: 72 anos");
-        } else if ((CbRaca.getSelectedItem() == "Grande") && (CbIdade.getSelectedItem() == ("12"))) {
+        } else if ((CbRaca.getSelectedItem() == "Grande") && (CbIdade.getSelectedItem() == ("12 Anos"))) {
             txtExibeIdade.setText("Estimativa: 77 anos");
-        } else if ((CbRaca.getSelectedItem() == "Grande") && (CbIdade.getSelectedItem() == ("13"))) {
+        } else if ((CbRaca.getSelectedItem() == "Grande") && (CbIdade.getSelectedItem() == ("13 Anos"))) {
             txtExibeIdade.setText("Estimativa: 82 anos");
-        } else if ((CbRaca.getSelectedItem() == "Grande") && (CbIdade.getSelectedItem() == ("14"))) {
+        } else if ((CbRaca.getSelectedItem() == "Grande") && (CbIdade.getSelectedItem() == ("14 Anos"))) {
             txtExibeIdade.setText("Estimativa: 88 anos");
-        } else if ((CbRaca.getSelectedItem() == "Grande") && (CbIdade.getSelectedItem() == ("15"))) {
+        } else if ((CbRaca.getSelectedItem() == "Grande") && (CbIdade.getSelectedItem() == ("15 Anos"))) {
             txtExibeIdade.setText("Estimativa: 93 anos");
-        } else if ((CbRaca.getSelectedItem() == "Grande") && (CbIdade.getSelectedItem() == ("16"))) {
+        } else if ((CbRaca.getSelectedItem() == "Grande") && (CbIdade.getSelectedItem() == ("16 Anos"))) {
             txtExibeIdade.setText("Estimativa: 120 anos");
         } else {
             txtExibeIdade.setText("Selecione Idade/Raça");
